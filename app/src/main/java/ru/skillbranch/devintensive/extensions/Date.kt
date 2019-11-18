cf2a49bbe0613be3e6c14f5e5ac7c2ec571e0ada
Egor.Ruban <Egor-Ruban@mail.ru>
2019-11-18 23:16:41 +0700

    fix Date extensions add TimeUnits Enum in Date

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -3,26 +3,30 @@ package ru.skillbranch.devintensive.extensions
 import java.lang.IllegalStateException
 import java.text.SimpleDateFormat
 import java.util.*
+import java.util.concurrent.TimeUnit
 import java.util.regex.Pattern
 
-const val SECOND = 1000L
-const val MINUTE = 60 * SECOND
-const val HOUR = 60 * MINUTE
-const val DAY = 24 * HOUR
+
+enum class TimeUnits(val time:Long){
+    SECOND(1000L),
+    MINUTE(60*SECOND.time),
+    HOUR(60*MINUTE.time),
+    DAY(24*HOUR.time)
+}
 
 fun Date.format(pattern: String="HH.mm.ss dd.MM.yy"):String{
+    Thread.sleep(1000L)
     val dateformat = SimpleDateFormat(pattern, Locale("ru"))
     return dateformat.format(this)
 }
 
-fun Date.add(value:Int, units:String):Date{
+fun Date.add(value:Int, units:TimeUnits):Date{
     var time = this.time
     time += when(units){
-        "seconds","second"->value* SECOND
-        "minutes","minute"->value* MINUTE
-        "hour","hours"->value* HOUR
-        "day","days"->value* DAY
-        else -> throw IllegalStateException("invalid unit")
+        TimeUnits.SECOND->value * TimeUnits.SECOND.time
+        TimeUnits.MINUTE->value* TimeUnits.MINUTE.time
+        TimeUnits.HOUR->value* TimeUnits.HOUR.time
+        TimeUnits.DAY->value* TimeUnits.DAY.time
     }
     this.time = time
     return this
