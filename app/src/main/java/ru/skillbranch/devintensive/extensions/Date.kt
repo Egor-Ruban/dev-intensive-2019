391853d96b3948cd87ecad0be0004447c4480e69
Egor.Ruban <Egor-Ruban@mail.ru>
2019-11-21 17:24:36 +0700

    add Plural method - extension for Date

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -11,13 +11,47 @@ enum class TimeUnits(val time:Long){
     SECOND(1000L),
     MINUTE(60*SECOND.time),
     HOUR(60*MINUTE.time),
-    DAY(24*HOUR.time)
+    DAY(24*HOUR.time);
+
+    fun plural(value:Int):String{
+        var typeOfPlural = 3
+        if((value%100)/10!=1 && value%10 == 1) typeOfPlural = 1
+        else if((value%100)/10!=1 && value%10 >=2 && value%10 <=4) typeOfPlural = 2
+        if(this == SECOND){
+            when(typeOfPlural){
+                1 -> return "$value секунду"
+                2 -> return "$value секунды"
+                3 -> return "$value секунд"
+            }
+        }
+        if(this == MINUTE){
+            when(typeOfPlural){
+                1 -> return "$value минуту"
+                2 -> return "$value минуты"
+                3 -> return "$value минут"
+            }
+        }
+        if(this == HOUR){
+            when(typeOfPlural){
+                1 -> return "$value час"
+                2 -> return "$value часа"
+                3 -> return "$value часов"
+            }
+        }
+        when(typeOfPlural){
+            1 -> return "$value день"
+            2 -> return "$value дня"
+            3 -> return "$value дней"
+        }
+        return "error"
+    }
 }
 
 fun Date.format(pattern: String="HH:mm:ss dd.MM.yy"):String{
     Thread.sleep(1000L)
-    val dateformat = SimpleDateFormat(pattern, Locale("ru"))
-    return dateformat.format(this)
+    val dateFormat = SimpleDateFormat(pattern, Locale("ru"))
+    return dateFormat.format(this)
+
 }
 
 fun Date.add(value:Int, units:TimeUnits):Date{
