0419c98555cb3f04f4bd4a51e749862847054a35
compucter <egor-ruban@mail.ru>
2020-02-05 21:45:36 +0700

    обавил CircleImageView

---
+++ app/src/main/java/ru/skillbranch/devintensive/ui/custom/CircleImageView.kt
@@ -0,0 +1,45 @@
+package ru.skillbranch.devintensive.ui.custom
+
+import android.content.Context
+import android.graphics.*
+import android.graphics.drawable.BitmapDrawable
+import android.util.AttributeSet
+import android.widget.ImageView
+
+
+
+class CircleImageView @JvmOverloads constructor(
+        context: Context,
+        attrs: AttributeSet? = null,
+        defStyleAttrs:Int=0
+) : ImageView(
+        context, attrs, defStyleAttrs
+){
+    override fun onDraw(canvas: Canvas) {
+        drawRoundImage(canvas)
+    }
+
+    private fun drawRoundImage(canvas: Canvas) {
+        var b: Bitmap = (drawable as BitmapDrawable).bitmap
+        val bitmap = b.copy(Bitmap.Config.ARGB_8888, true)
+
+        /* Scale the bitmap */
+        val scaledBitmap: Bitmap
+        val ratio: Float = bitmap.width.toFloat() / bitmap.height.toFloat()
+        val height: Int = Math.round(width / ratio)
+        scaledBitmap = Bitmap.createScaledBitmap(bitmap, width, height, false)
+
+        /* Cutting the outer of the circle */
+        val shader: Shader
+        shader = BitmapShader(scaledBitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP)
+
+        val rect = RectF()
+        rect.set(0f, 0f, width.toFloat(), height.toFloat())
+
+        val imagePaint = Paint()
+        imagePaint.isAntiAlias = true
+        imagePaint.shader = shader
+        canvas.drawRoundRect(rect, width.toFloat(), height.toFloat(), imagePaint)
+    }
+
+}
